dfdbddeff6e824c4a16919d2d20d3d3dead70714
intameli <intameli@gmail.com>
2020-04-17 15:50:39 +1000

    Updates

---
+++ precache-manifest.2de5424a563a5601965aa4db80e1a090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8195e74159dff9b6fe8aedeb501cb003",
+    "url": "/play/index.html"
+  },
+  {
+    "revision": "6d1e9fe3f0df0a0140a6",
+    "url": "/play/static/css/main.4b3b8dad.chunk.css"
+  },
+  {
+    "revision": "21a9a095cf6112a46fd1",
+    "url": "/play/static/js/2.9e8d2959.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/play/static/js/2.9e8d2959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1e9fe3f0df0a0140a6",
+    "url": "/play/static/js/main.7ac38c16.chunk.js"
+  },
+  {
+    "revision": "37b0c2c08ebb0e866f41",
+    "url": "/play/static/js/runtime-main.3ec80c99.js"
+  }
+]);
